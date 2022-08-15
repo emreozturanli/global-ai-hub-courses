e4b1f63e2be5b2b7341cd1ab640df203e76a0b84
emre <emreozturanli1@gmail.com>
2022-08-15 21:20:29 +0300

    my courses is done

--- src/components/MyCard.tsx
+++ src/components/MyCard.tsx
@@ -1,17 +1,35 @@
+import { useState } from "react"
 import { IMyCourse } from "../types/interfaces"
 
 interface IMyCardProps {
     course: IMyCourse
 }
 
-const MyCard = (props:IMyCardProps) => {
-    const {course} = props
-  return (
-    <div className="card">
-        <img src={course.card_image} alt={course.title} />
-        <h2>{course.title}</h2>
-    </div>
-  )
+const MyCard = (props: IMyCardProps) => {
+    const [modal, setModal] = useState<boolean>(false)
+    const { course } = props
+    console.log(modal)
+    return (
+        <>
+            <div className="card" onClick={() => setModal(true)}>
+                <img src={course.card_image} alt={course.title} />
+                <h2>{course.title}</h2>
+            </div>
+            <div className={modal ? "modal open" : "modal"}>
+                <div className="modal-content">
+                    <img src={course.card_image} alt={course.title} />
+                    <h2>{course.title}</h2>
+                    <p dangerouslySetInnerHTML={{ __html: course.description }} />
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati voluptatem atque dolor minus nam, ipsa necessitatibus odio a perferendis beatae quas excepturi minima ducimus quaerat, aut veniam quod tempora. Repellendus cupiditate perspiciatis adipisci delectus reiciendis quae voluptatem facere, eveniet corporis, vero dolorum ad saepe animi atque cumque recusandae exercitationem nemo.</p>
+                </div>
+                <button
+                    className="close"
+                    onClick={() => setModal(false)}
+                >&#10006;
+                </button>
+            </div>
+        </>
+    )
 }
 
 export default MyCard
