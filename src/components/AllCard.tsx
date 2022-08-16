bfee3bf3ebdd5e4c92283d67a013141995b2300d
emre <emreozturanli1@gmail.com>
2022-08-16 12:31:46 +0300

    referrer added

--- src/components/AllCard.tsx
+++ src/components/AllCard.tsx
@@ -33,12 +33,13 @@ const AllCard = (props: IAllCardProps) => {
         <img className={extraInfo?.guid.rendered === undefined ? 'gif': ''}
         src={ extraInfo?.guid.rendered !== undefined ? extraInfo?.guid.rendered : spinner} 
         alt={course.title.rendered} 
+        referrerPolicy='no-referrer'
         />
         <h2 dangerouslySetInnerHTML={{__html:course.title.rendered}}/>
     </div>
     <div className={modal ? "modal open" : "modal"}>
         <div className="modal-content" >
-        <img src={ extraInfo?.guid.rendered} alt={course.title.rendered} />
+        <img src={ extraInfo?.guid.rendered} alt={course.title.rendered} referrerPolicy='no-referrer' />
         <h2 dangerouslySetInnerHTML={{__html:course.title.rendered}}/>
         <p>Her pazarlama faaliyetinin gerçek bir tüketici içgörüsüne dayandırma gerekliliği, pazarlama ve iletişim dünyasının son 220 yıldır üzerinde en çok durduğu konulardan biri.  Bu “insan gerçeği” asla sadece rakamsal bilgi ve bulgular biçiminde olmamalıdır.Bilgi ve bulgular önemlidir ancak çoğunlukla kategorinin tüm markalarının elinde aynı bilgiler mevcuttur ve haliyle buradan yola çıkarak farklılaşma alanı bulmak zordur.  “İnsan gerçeğine” ulaşmak, tüketmeye sevk eden ancak tamamıyla keşfedilmemiş derin etkenlerin keşfi ile başlar.Akan Abdula “Büyük Veriden İçgörü Bulma” başlıklı sunumuyla büyük veri dünyasından başlayarak, içgörü bulmanın inceliklerini anlatacak.</p>
         </div>
--- src/components/MyCard.tsx
+++ src/components/MyCard.tsx
@@ -11,12 +11,12 @@ const MyCard = (props: IMyCardProps) => {
     return (
         <>
             <div className="card" onClick={() => setModal(true)}>
-                <img src={course.card_image} alt={course.title} />
+                <img src={course.card_image} alt={course.title}  referrerPolicy='no-referrer'/>
                 <h2>{course.title}</h2>
             </div>
             <div className={modal ? "modal open" : "modal"}>
                 <div className="modal-content">
-                    <img src={course.card_image} alt={course.title} />
+                    <img src={course.card_image} alt={course.title} referrerPolicy='no-referrer'/>
                     <h2>{course.title}</h2>
                     <p dangerouslySetInnerHTML={{ __html: course.description }} />
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati voluptatem atque dolor minus nam, ipsa necessitatibus odio a perferendis beatae quas excepturi minima ducimus quaerat, aut veniam quod tempora.</p>
