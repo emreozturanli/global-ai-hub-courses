347d4671f5bb6986e49f128dd0eac3a946139fb0
emre <emreozturanli1@gmail.com>
2022-08-16 10:07:10 +0300

    active btn is done

--- src/components/AllCard.tsx
+++ src/components/AllCard.tsx
@@ -2,7 +2,7 @@ import axios from "axios"
 import { useEffect, useState } from "react"
 import { IAllCourse } from "../types/allCoursesInterfaces"
 import { IExtraInfo } from "../types/extraInfoInterfaces"
-
+import spinner from "../assets/spinner.gif"
 
 interface IAllCardProps {
     course: IAllCourse
@@ -30,11 +30,18 @@ const AllCard = (props: IAllCardProps) => {
   return (
     <>
     <div className="card" onClick={() => setModal(true)}>
-        <img src={extraInfo?.guid.rendered} alt={course.title.rendered} />
+        <img className={extraInfo?.guid.rendered === undefined ? 'gif': ''}
+        src={ extraInfo?.guid.rendered !== undefined ? extraInfo?.guid.rendered : spinner} 
+        alt={course.title.rendered} 
+        />
         <h2 dangerouslySetInnerHTML={{__html:course.title.rendered}}/>
     </div>
     <div className={modal ? "modal open" : "modal"}>
-        <div className="modal-content" dangerouslySetInnerHTML={{__html:course?.content.rendered}}/>
+        <div className="modal-content" >
+        <img src={ extraInfo?.guid.rendered} alt={course.title.rendered} />
+        <h2 dangerouslySetInnerHTML={{__html:course.title.rendered}}/>
+        <p>Her pazarlama faaliyetinin gerçek bir tüketici içgörüsüne dayandırma gerekliliği, pazarlama ve iletişim dünyasının son 220 yıldır üzerinde en çok durduğu konulardan biri.  Bu “insan gerçeği” asla sadece rakamsal bilgi ve bulgular biçiminde olmamalıdır.Bilgi ve bulgular önemlidir ancak çoğunlukla kategorinin tüm markalarının elinde aynı bilgiler mevcuttur ve haliyle buradan yola çıkarak farklılaşma alanı bulmak zordur.  “İnsan gerçeğine” ulaşmak, tüketmeye sevk eden ancak tamamıyla keşfedilmemiş derin etkenlerin keşfi ile başlar.Akan Abdula “Büyük Veriden İçgörü Bulma” başlıklı sunumuyla büyük veri dünyasından başlayarak, içgörü bulmanın inceliklerini anlatacak.</p>
+        </div>
         <button
             className="close"
             onClick={() => setModal(false)}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,27 @@
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import logo from '../assets/logo.webp'
 
 const Header = () => {
+    const [active,setActive] = useState(true)
     const navigate=useNavigate()
+    
     return (
         <header>
             <nav>
                 <img src={logo} alt="logo" className='logo' />
                 <div className="btn-group">
-                    <button onClick={()=> navigate('/')}>My Courses</button>
-                    <button onClick={()=> navigate('/all-courses')}>All Courses</button>
+                    <button 
+                    className={active ? 'active' : ''}
+                    onClick={()=>{navigate('/'); setActive(true)}}
+
+                    >
+                        My Courses</button>
+                    <button 
+                    className={!active ? 'active' : ''}
+                    onClick={()=> {navigate('/all-courses'); setActive(false)}}
+                    >
+                        All Courses</button>
                 </div>
             </nav>
         </header>
